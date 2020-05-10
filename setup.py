9ade674443afdaa61a706a23670bc84fa7aa55a6
jieyu <code@jieyu.ai>
2020-05-10 20:41:15 +0800

    Bump version: 0.4.1 → 0.4.2

--- setup.py
+++ setup.py
@@ -42,6 +42,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/hbaaron/pyemit',
-    version='0.4.1',
+    version='0.4.2',
     zip_safe=False,
 )
